c4f326566d7df41f35317cb99966ccedebf71cd1
Kyrylo Silin <silin@kyrylo.org>
2016-08-06 16:19:38 +0300

    Client: connect to WebSocket Server

--- js/src/main/scala/Client.scala
+++ js/src/main/scala/Client.scala
@@ -1,8 +1,11 @@
 import scala.scalajs.js.JSApp
 import org.scalajs.dom
+import org.scalajs.dom.raw._
+import scala.scalajs.js.Dynamic.global
 import dom.document
 
 object Client extends JSApp {
+  private final val WSServer = "ws://localhost:8080/ws-echo"
 
   def appendPar(targetNode: dom.Node, text: String): Unit = {
     val parNode = document.createElement("p")
@@ -14,6 +17,20 @@ object Client extends JSApp {
   def main(): Unit = {
     appendPar(document.body, "Hello World")
     appendPar(document.body, SuccessfulCandidate("http://www.example.com").toString())
-  }
 
+    val socket = new WebSocket(WSServer)
+
+    socket.onopen = (e: scala.scalajs.js.Any) => {
+      global.console.log("Connected to WebSocket Server on " ++ WSServer)
+      socket.send("search")
+    }
+
+    socket.onerror = (e: dom.ErrorEvent) => {
+      global.console.log("Error: " ++ e.toString())
+    }
+
+    socket.onmessage = (e: dom.MessageEvent) => {
+      global.console.log(e)
+    }
+  }
 }
