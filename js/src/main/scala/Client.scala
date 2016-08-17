217f436ac83f7a3665a7c45ff758d33618dc417b
Serhii Pererva <sergey.pererva@gmail.com>
2016-08-17 13:54:44 +0300

    better way of handling websocket connection

--- js/src/main/scala/Client.scala
+++ js/src/main/scala/Client.scala
@@ -1,11 +1,16 @@
 import scala.scalajs.js.JSApp
+import org.scalajs.jquery.JQueryEventObject
 import org.scalajs.dom
 import org.scalajs.dom.raw._
 import org.scalajs.jquery.{jQuery => JQ}
 import scala.scalajs.js.Dynamic.global
 import scala.scalajs.js.timers._
+import scala.scalajs.js.{Any}
 import dom.document
 import upickle.default._
+import scala.concurrent.{Promise, Future}
+import scala.scalajs.concurrent.JSExecutionContext.Implicits.queue
+import scala.collection.mutable
 
 object Client extends JSApp {
   private final val WSServer = "ws://localhost:8080/ws-echo"
@@ -21,47 +26,89 @@ object Client extends JSApp {
   }
 
   def main(): Unit = {
-    val socket = new WebSocket(WSServer)
     val links = document.getElementById("links")
     var progress: Option[Progress] = None
 
-    socket.onopen = (e: scala.scalajs.js.Any) => {
-      global.console.log("Connected to WebSocket Server on " + WSServer)
-    }
+    def bindConnectionEvents(conn: Connection) = {
 
-    socket.onerror = (e: dom.Event) => {
-      global.console.log("Error occurred:", e)
-    }
+      conn.open onSuccess { case _ => Progress.show }
+      conn.close onComplete { case _ => Progress.remove }
 
-    socket.onmessage = (e: dom.MessageEvent) =>
-      read[Tuple2[String, String]](e.data.toString()) match {
-        case ("SuccessfulCandidate", url) =>
-          progress foreach { _.progress }
+      conn
+        .onError { e: dom.Event => global.console.error("Error occured", e) }
+        .onMessage("SuccessfulCandidate") { url =>
           appendCandidate(links, url)
-        case ("FailedCandidate", problem) => progress foreach { _.progress }
-        case ("CandidatesCount", countString) => progress = Some(new Progress(countString.toInt))
-        case _ => global.console.error("unknown message", e)
-      }
+          progress foreach { _.progress }
+        }
+        .onMessage("FailedCandidate") { _ => progress foreach { _.progress } }
+        .onMessage("CandidatesCount") { countString =>
+          progress = Some(new Progress(countString.toInt))
+        }
+    }
 
-    socket.onclose = (e: dom.Event) => Progress.remove
+    JQ {
+      JQ("#search-btn").on("click", { e: JQueryEventObject =>
+        val conn = new Connection()
+        bindConnectionEvents(conn)
+        conn.send("StartSearch", document.getElementById("search-box") match {
+          case elem: HTMLInputElement => elem.value
+          case elem =>
+            global.console.error(s"expected HTMLInputElement, got $elem")
+            throw new RuntimeException()
+        })
+      })
+    }
+  }
+}
 
-    document.addEventListener("DOMContentLoaded", { (e: dom.Event) =>
-      val searchBox = document.getElementById("search-box") match {
-        case (elem: HTMLInputElement) =>
-          elem
-        case elem =>
-          val err = s"expected HTMLInputElement, got $elem"
-          global.console.error(err)
-          throw new RuntimeException(err)
-      }
-      val searchBtn = document.getElementById("search-btn")
+class Connection(private val url: String = "ws://localhost:8080/ws-echo") {
 
-      searchBtn.addEventListener("click", { (mouseEvent: dom.Event) =>
-        socket.send(write(("StartSearch", searchBox.value)))
-        links.innerHTML = ""
-        Progress.show
-      }, false)
-    });
+  private type MessageHandler = String => Unit
+  private type ErrorHandler = dom.Event => Unit
+
+  private val openPromise = Promise[Any]()
+  private val closedPromise = Promise[dom.Event]()
+  private val socketPromise = Promise[WebSocket]()
+  private val onMessageCallbacks: mutable.Map[String, List[MessageHandler]] =
+    mutable.Map.empty withDefault { _ => List.empty }
+  private val onErrorCallbacks: mutable.ListBuffer[ErrorHandler] =
+    mutable.ListBuffer.empty
+
+  private def connect: WebSocket = {
+    val socket = new WebSocket(url)
+    socketPromise.success(socket)
+    socket.onopen = (e: Any) => openPromise.success(e)
+    socket.onclose = (e: dom.Event) => closedPromise.success(e)
+    socket.onmessage = (e: dom.MessageEvent) => {
+      val (messageType, content) = read[(String, String)](e.data.toString())
+      onMessageCallbacks(messageType) foreach { _(content) }
+    }
+    socket.onerror = (e: dom.Event) => onErrorCallbacks.toList foreach { _(e) }
+
+    socket
+  }
+
+  private def socket: Future[WebSocket] = {
+    if (!socketPromise.isCompleted) connect
+    // there is no point of having socket instance
+    // if it's connection is not yet opened
+    open flatMap { _ => socketPromise.future }
+  }
+
+  def open = openPromise.future
+
+  def close = closedPromise.future
+
+  def onMessage(messageType: String)(h: MessageHandler) = {
+    onMessageCallbacks.update(messageType, h :: onMessageCallbacks(messageType))
+    this
+  }
+
+  def onError(h: ErrorHandler) = { onErrorCallbacks += h; this }
+
+  def send(messageType: String, data: String) = socket foreach {
+    case socket: WebSocket => socket.send(write((messageType, data)))
+
   }
 }
 
